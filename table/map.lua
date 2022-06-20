e1d951febe9bb061d10a471b7126cdeb93a30373
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-20 19:19:19 +0200

    ♻️ update map method

--- table/map.lua
+++ table/map.lua
@@ -1,4 +1,4 @@
-local function map(array, callback)
+table.map = function(array, callback)
   local result = {}
   for index, value in pairs(array) do
     table.insert(result, callback(value, index, array))
